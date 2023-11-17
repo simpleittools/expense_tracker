3f0ba6b29bc071f1183ccf00f6cd2012f4a5e49c
ryan <wm.ryan.mooney@gmail.com>
2023-11-17 08:21:01 -0900

    Add list via enu. build new stateless class to store the list in expenses_list.dart. display the list via an expanded widget in expenses.dart

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,7 @@
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:flutter/material.dart';
+
+import 'models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,13 +13,38 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure),
+    Expense(
+        title: 'Flight',
+        amount: 578.0,
+        date: DateTime.now(),
+        category: Category.travel),
+    Expense(
+        title: 'Sushi',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.food),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text("The Chart"),
-          Text('expenses list'),
+          const Text("The Chart"),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
