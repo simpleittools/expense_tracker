f59ba8466a09a513b6c6eb5e0bf8b0777504f133
ryan <wm.ryan.mooney@gmail.com>
2023-11-21 10:15:42 -0900

    Build the Charts and integrate with the expenses.dart. Build the new ExpenseBucket Class in expense.dart.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -100,7 +101,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text("The Chart"),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           ),
