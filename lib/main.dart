127bf9e3f951d9f578c8a23f5c65d070fac41b16
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-11-20 15:16:11 -0900

    add color scheme via main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+// k at the beginning of the variable implies global variable. it is not required, but it is convention.
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(
+    255,
+    0,
+    121,
+    107,
+  ),
+);
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: const Expenses(),
     ),
   );
